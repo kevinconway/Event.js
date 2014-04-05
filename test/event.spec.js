3ddaddd9161e685331d7742e4f0749818bc2cdef
Kevin Conway <kevinjacobconway@gmail.com>
2014-04-05 01:19:43 -0500

    Change: Test module pattern to match project
    Signed-off-by: Kevin Conway <kevinjacobconway@gmail.com>

--- test/event.spec.js
+++ test/event.spec.js
@@ -1,223 +1,188 @@
-/*global require, define, module, describe, it, xit
+/*jslint node: true, indent: 2, passfail: true */
+/*globals describe, it */
 
-*/
-(function (ctx, factory) {
-    "use strict";
+(function (context, generator) {
+  "use strict";
 
-    var env = factory.env,
-        def = factory.def,
-        deps = {
-            amd: ['lib/expect', '../event/node_modules/modelo/modelo.js', '../event/event'],
-            node: ['./lib/expect', 'modelo', '../event/event.js'],
-            browser: ['expect', 'Modelo', 'Event']
-        };
+  generator.call(
+    context,
+    'tests/eventjs',
+    ['expect', 'eventjs'],
+    function (expect, EventMixin) {
 
-    def.call(ctx, 'spec/Event', deps[env], function (expect, Modelo, EventMixin) {
+      describe('Event.js', function () {
 
-        describe('Event.js', function () {
+        it('loads in the current environment', function () {
 
-            it('loads in the current environment (' + env + ')', function () {
-
-                expect(EventMixin).to.be.ok();
-
-            });
-
-            it('exposes a specification compliant interface', function () {
-
-                expect(typeof EventMixin).to.be("function");
-
-                expect(typeof EventMixin.extend).to.be("function");
-
-            });
-
-            it('registers and triggers events', function (done) {
-
-                var t = new EventMixin(),
-                    test_value = {};
-
-                t.on('test', function () {
-                    test_value.test = true;
-                    expect(test_value.test).to.be(true);
-                    done();
-                });
-
-                expect(test_value.test).to.be(undefined);
-
-                t.trigger('test');
-
-                expect(test_value.test).to.be(undefined);
-
-            });
-
-            it('removes events', function (done) {
-
-                var t = new EventMixin(),
-                    test_value = {},
-                    callback = function () {
-                        test_value.test = true;
-                        expect(test_value.test).to.be(true);
-
-                        t.off('test', callback);
-
-                        expect(test_value.test).to.be(true);
-
-                        test_value.test = false;
-
-                        expect(test_value.test).to.be(false);
-
-                        t.on('test', function () {
-
-                            expect(test_value.test).to.be(false);
-
-                            done();
-
-                        });
-
-                        t.trigger('test');
-
-                    };
-
-                t.on('test', callback);
-
-                expect(test_value.test).to.be(undefined);
-
-                t.trigger('test');
-
-                expect(test_value.test).to.be(undefined);
-
-            });
-
-            it('removes events of the same callback but different context correctly', function () {
-
-                var t = new EventMixin(),
-                    callback = function () {},
-                    fakeCtx = function () {};
-
-                t.on('test', callback, fakeCtx);
-                t.on('test', callback, fakeCtx);
-                t.on('test', callback);
-                t.on('test', callback);
-
-                expect(t.events.test.length).to.be(4);
-
-                t.off('test', callback, fakeCtx);
-                expect(t.events.test.length).to.be(2);
-
-                t.off('test', callback);
-                expect(t.events.test.length).to.be(0);
-
-            });
+          expect(EventMixin).to.be.ok();
 
         });
 
-    });
+        it('exposes a specification compliant interface', function () {
 
-}(this, (function (ctx) {
-    "use strict";
+          expect(typeof EventMixin).to.be("function");
 
-    var currentEnvironment,
-        generator;
+          expect(typeof EventMixin.extend).to.be("function");
 
-    // Check the environment to determine the dependency management strategy.
+        });
 
-    if (typeof define === "function" && !!define.amd) {
+        it('registers and triggers events', function (done) {
 
-        currentEnvironment = 'amd';
+          var t = new EventMixin(),
+            test_value = {};
 
-    } else if (typeof require === "function" &&
-                        module !== undefined && !!module.exports) {
+          t.on('test', function () {
+            test_value.test = true;
+            expect(test_value.test).to.be(true);
+            done();
+          });
 
-        currentEnvironment = 'node';
+          expect(test_value.test).to.be(undefined);
 
-    } else if (ctx.window !== undefined) {
+          t.trigger('test');
 
-        currentEnvironment = 'browser';
+          expect(test_value.test).to.be(undefined);
 
+        });
+
+        it('removes events', function (done) {
+
+          var t = new EventMixin(),
+            test_value = {},
+            callback = function () {
+              test_value.test = true;
+              expect(test_value.test).to.be(true);
+
+              t.off('test', callback);
+
+              expect(test_value.test).to.be(true);
+
+              test_value.test = false;
+
+              expect(test_value.test).to.be(false);
+
+              t.on('test', function () {
+
+                expect(test_value.test).to.be(false);
+
+                done();
+
+              });
+
+              t.trigger('test');
+
+            };
+
+          t.on('test', callback);
+
+          expect(test_value.test).to.be(undefined);
+
+          t.trigger('test');
+
+          expect(test_value.test).to.be(undefined);
+
+        });
+
+        it('removes events of the same callback but different context correctly', function () {
+
+          var t = new EventMixin(),
+            callback = function () { return null; },
+            fakeCtx = function () { return null; };
+
+          t.on('test', callback, fakeCtx);
+          t.on('test', callback, fakeCtx);
+          t.on('test', callback);
+          t.on('test', callback);
+
+          expect(t.events.test.length).to.be(4);
+
+          t.off('test', callback, fakeCtx);
+          expect(t.events.test.length).to.be(2);
+
+          t.off('test', callback);
+          expect(t.events.test.length).to.be(0);
+
+        });
+
+      });
     }
+  );
 
-    generator = (function () {
-        switch (currentEnvironment) {
+}(this, (function (context) {
+  "use strict";
 
-        case 'amd':
+  // Ignoring the unused "name" in the Node.js definition function.
+  /*jslint unparam: true */
+  if (typeof require === "function" &&
+        module !== undefined &&
+        !!module.exports) {
 
-            // If RequireJS is used to load this module then return the global
-            // define() function.
-            return function (name, deps, mod) {
-                define(deps, mod);
-            };
+    // If this module is loaded in Node, require each of the
+    // dependencies and pass them along.
+    return function (name, deps, mod) {
 
-        case 'node':
+      var x,
+        dep_list = [];
 
-            // If this module is loaded in Node, require each of the
-            // dependencies and pass them along.
-            return function (name, deps, mod) {
+      for (x = 0; x < deps.length; x = x + 1) {
 
-                var x,
-                    dep_list = [];
+        dep_list.push(require(deps[x]));
 
-                for (x = 0; x < deps.length; x = x + 1) {
+      }
 
-                    dep_list.push(require(deps[x]));
+      module.exports = mod.apply(context, dep_list);
 
-                }
+    };
 
-                module.exports = mod.apply(this, dep_list);
+  }
+  /*jslint unparam: false */
 
-            };
+  if (context.window !== undefined) {
 
-        case 'browser':
+    // If this module is being used in a browser environment first
+    // generate a list of dependencies, run the provided definition
+    // function with the list of dependencies, and insert the returned
+    // object into the global namespace using the provided module name.
+    return function (name, deps, mod) {
 
-            // If this module is being used in a browser environment first
-            // generate a list of dependencies, run the provided definition
-            // function with the list of dependencies, and insert the returned
-            // object into the global namespace using the provided module name.
-            return function (name, deps, mod) {
+      var namespaces = name.split('/'),
+        root = context,
+        dep_list = [],
+        current_scope,
+        current_dep,
+        i,
+        x;
 
-                var namespaces = name.split('/'),
-                    root = this,
-                    dep_list = [],
-                    current_scope,
-                    current_dep,
-                    i,
-                    x;
+      for (i = 0; i < deps.length; i = i + 1) {
 
-                for (i = 0; i < deps.length; i = i + 1) {
+        current_scope = root;
+        current_dep = deps[i].split('/');
 
-                    current_scope = root;
-                    current_dep = deps[i].split('/');
+        for (x = 0; x < current_dep.length; x = x + 1) {
 
-                    for (x = 0; x < current_dep.length; x = x + 1) {
-
-                        current_scope = current_scope[current_dep[x]] || {};
-
-                    }
-
-                    dep_list.push(current_scope);
-
-                }
-
-                current_scope = root;
-                for (i = 1; i < namespaces.length; i = i + 1) {
-
-                    current_scope = current_scope[namespaces[i - 1]] || {};
-
-                }
-
-                current_scope[namespaces[i - 1]] = mod.apply(this, dep_list);
-
-            };
-
-        default:
-            throw new Error("Unrecognized environment.");
+          current_scope = current_scope[current_dep[x]] =
+                          current_scope[current_dep[x]] || {};
 
         }
 
-    }());
+        dep_list.push(current_scope);
 
+      }
 
-    return {
-        env: currentEnvironment,
-        def: generator
+      current_scope = root;
+      for (i = 1; i < namespaces.length; i = i + 1) {
+
+        current_scope = current_scope[namespaces[i - 1]] =
+                        current_scope[namespaces[i - 1]] || {};
+
+      }
+
+      current_scope[namespaces[i - 1]] = mod.apply(context, dep_list);
+
     };
+
+  }
+
+  throw new Error("Unrecognized environment.");
 
 }(this))));
